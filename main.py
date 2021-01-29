7b845f4e3d6428603b976bb62e11f4a3b765bbd9
aliraza7926 <aliraza7926@gmail.com>
2021-01-29 11:29:36 +0330

    change varible name dict to my_dict and reformat the file

--- main.py
+++ main.py
@@ -5,23 +5,13 @@ def normalize(text):
     text = text.replace(".", " ")
     text = text.replace("\"", " ")
     text = text.replace("?", " ")
-    text = text.replace("\n"," ")
-    text= text.replace("("," ")
-    text = text.replace(")"," ")
+    text = text.replace("\n", " ")
+    text = text.replace("(", " ")
+    text = text.replace(")", " ")
     text = text.lower()
 
     return text
 
-
-#
-# f=open("wasteland.txt","r")
-# poem=f.read()
-# f.close()
-#
-# poem= normalize(poem)
-# g=open("new.txt","w")
-# g.write(poem)
-# g.close()
 
 def count_all_words(text):
     return len(text.split(" "))
@@ -29,6 +19,7 @@ def count_all_words(text):
 
 def count_words(text):
     return len(set(text.split(" ")))
+
 
 def answer1(text):
     f = open("out_words_count.txt", "w")
@@ -39,42 +30,44 @@ def answer1(text):
 
 
 def word_frequency(text):
-    dict = {}
-    all_words=text.split(" ")
-    unique_wrods=set(text.split(" "))
+    my_dict = {}
+    all_words = text.split(" ")
+    unique_wrods = set(text.split(" "))
     for word in unique_wrods:
-        dict[word]=all_words.count(word)
+        my_dict[word] = all_words.count(word)
 
-    del dict["" ""]
-    return dict
+    del my_dict["" ""]
+    return my_dict
+
 
 def answer3(text):
-    dict = word_frequency(text)
-    key = list(dict.keys())
-    value= list(dict.values())
-    f=open("out_most_frequent.txt","w")
+    my_dict = word_frequency(text)
+    key = list(my_dict.keys())
+    value = list(my_dict.values())
+    f = open("out_most_frequent.txt", "w")
 
-    for i in range(1,11):
-        f.write(str(i)+". "+key[value.index(max(value))]+"\t"+str(value[value.index(max(value))])+"\n")
+    for i in range(1, 11):
+        f.write(str(i) + ". " + key[value.index(max(value))] + "\t" + str(value[value.index(max(value))]) + "\n")
         key.pop(value.index(max(value)))
         value.pop(value.index(max(value)))
 
     f.close()
 
+
 def answer4(text):
-    dict = word_frequency(text)
+    my_dict = word_frequency(text)
 
-    new_dict={}
-    for i in range(1,6):
-        max_length=0
-        max_key=""
-        for key in dict:
-            if(len(key)>max_length):
-                max_key=key
-                max_length=len(key)
+    new_dict = {}
+    for i in range(1, 6):
+        max_length = 0
+        max_key = ""
+        for key in my_dict:
+            if (len(key) > max_length):
+                max_key = key
+                max_length = len(key)
 
-        new_dict[max_key]=dict[max_key]
-        del dict[max_key]
+        new_dict[max_key] = my_dict[max_key]
+        del my_dict[max_key]
 
     return new_dict
 
@@ -83,7 +76,7 @@ f = open("test.txt", "r")
 poem = f.read()
 f.close()
 
-normalized_poem= normalize(poem)
+normalized_poem = normalize(poem)
 answer1(normalized_poem)
 answer3(normalized_poem)
 answer4(normalized_poem)
