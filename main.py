a4645879e373923fe6dfa7e7c0ea9fa11513d273
aliraza7926 <aliraza7926@gmail.com>
2021-01-29 12:09:23 +0330

    write answer for question5

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def normalize(text):
     text = text.replace("\n", " ")
     text = text.replace("(", " ")
     text = text.replace(")", " ")
+    text = text.replace("!", " ")
     text = text.lower()
 
     return text
@@ -72,7 +73,51 @@ def answer4(text):
     return new_dict
 
 
-f = open("test.txt", "r")
+def normalize_for_q5(text):
+    text = text.replace(",", " ")
+    text = text.replace("⁠", " ")
+    text = text.replace(";", " ")
+    text = text.replace(".", " ")
+    text = text.replace("\"", " ")
+    text = text.replace("?", " ")
+    text = text.replace("\n", " ")
+    text = text.replace("(", " ")
+    text = text.replace(")", " ")
+    text = text.replace("!", " ")
+
+    return text
+
+
+def is_capital(text):
+    if (text != '' and text != " "):
+        return text[0].isupper()
+    else:
+        return False
+
+
+def find_capital_words(text):
+    my_dict = {}
+    all_words = text.split(" ")
+    unique_wrods = set(text.split(" "))
+    for word in unique_wrods:
+        if (is_capital(word)):
+            my_dict[word] = all_words.count(word)
+
+    return my_dict
+
+
+def answer5(text):
+    normalized_text = normalize_for_q5(text)
+    my_dict = find_capital_words(normalized_text)
+    f = open("out_capital_words.txt", "w")
+
+    for key in my_dict:
+        f.write(key + " (" + str(my_dict[key]) + ")\n")
+
+    f.close()
+
+
+f = open("wasteland.txt", "r")
 poem = f.read()
 f.close()
 
@@ -80,3 +125,4 @@ normalized_poem = normalize(poem)
 answer1(normalized_poem)
 answer3(normalized_poem)
 answer4(normalized_poem)
+answer5(poem)
